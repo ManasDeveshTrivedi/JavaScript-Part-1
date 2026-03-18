3258da3329017fa510ed1730fbf72155a93d8783
Manas <manasdevtrivedi@gmail.com>
2026-03-19 00:09:54 +0530

    Added

---
+++ dataType-Summary.js
@@ -0,0 +1,24 @@
+/*
+Primitive:-
+7 types:String,Number,Boolean,null,undefined,Symbol,bigint
+*/
+let id=Symbol("123")
+let anotherId=Symbol("123")
+console.log(id==anotherId);
+
+const hero=["batman","superman","ironman","spiderman"]
+let myObj={
+    Name: "Manas",
+    age: 22,
+}
+
+const myFunction= function(){
+    console.log('Hello World');
+}
+console.log(typeof id);
+
+
+
+/*
+Reference(Non Primitive):Array, Objects, Functions
+*/
