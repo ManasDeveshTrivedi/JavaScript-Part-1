075ba98141226cc821c00c84f811ddabbad097af
Manas <manasdevtrivedi@gmail.com>
2026-03-18 23:35:41 +0530

    Added

--- conversion.js
+++ conversion.js
@@ -12,3 +12,13 @@ console.log(nameInName);
 //"" => False
 //"Manas" => True
 
+/*********Operations*********/
+let val=3
+let negVal=-val
+console.log(negVal);
+console.log(2**3);
+let str1="Manas"
+let str2=" Trivedi"
+let str3=str1 +str2
+console.log(str3);
+
