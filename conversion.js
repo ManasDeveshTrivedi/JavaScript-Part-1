0218f6f1b2eed8849a54b414222d343b1b67be33
Manas <manasdevtrivedi@gmail.com>
2026-03-18 23:24:19 +0530

    Added

---
+++ conversion.js
@@ -0,0 +1,14 @@
+// let score=33;
+// console.log(typeof score);
+// let valueInNumber=String(score);// number to string conversion
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+let name="Manas";
+console.log(typeof name);
+let nameInName=Number(name);
+console.log(nameInName);
+//"" => False
+//"Manas" => True
+
