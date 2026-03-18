47410b8e961c47eadc7614a139caf6fb6e254c63
Manas <manasdevtrivedi@gmail.com>
2026-03-19 00:28:46 +0530

    Added

--- dataType-Summary.js
+++ dataType-Summary.js
@@ -12,12 +12,27 @@ let myObj={
     age: 22,
 }
 
-const myFunction= function(){
+const myFunction= function(){// Object in JS
     console.log('Hello World');
 }
 console.log(typeof id);
 
+let myName='Manas'
+let anotherName=myName
+anotherName='Dev' // new value is stored earlier one is not used
+console.log(myName);
+console.log(anotherName);
 
+let userOne={
+    email: 'manas@balleballe.com',
+    upi:'user@ybl'
+}
+
+let userTwo=userOne
+
+userTwo.email="manas@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);
 
 /*
 Reference(Non Primitive):Array, Objects, Functions
