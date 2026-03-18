fe2c8decc0dac6e4f2b256ca678184b433157fb1
Manas <manasdevtrivedi@gmail.com>
2026-03-18 22:45:36 +0530

    Added

---
+++ DataTypes.js
@@ -0,0 +1,9 @@
+"use strict" // for all JS code as newer version 
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null =>standalone value
+//undefined=>
+//symbol=>unique
+
