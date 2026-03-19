c6f2295cec33dcb3d617997f8f2f4b1f9db0178b
Manas <manasdevtrivedi@gmail.com>
2026-03-19 13:11:59 +0530

    Added

---
+++ Strings.js
@@ -0,0 +1,26 @@
+const name='Manas';
+const repoCount=5;
+// console.log(name+repoCount+"Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+const gameName=new String('  RE requiem')
+console.log(gameName);
+console.log(gameName.toUpperCase());
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('r'));
+console.log(gameName.trim());
+console.log(gameName.replace('requiem','Village'))
+console.log(gameName.includes('RE'));
+console.log(gameName.split(' '));
+
+// const myObj={
+//     name:"Manas",
+//     age:21
+// }
+// console.log(myObj.__proto__);
+
+
+
+//  Always use backticks whenever u need to print is efficient``
