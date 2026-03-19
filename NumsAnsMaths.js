fb11d01464cd09d63793966b6904f9f388d59f54
Manas <manasdevtrivedi@gmail.com>
2026-03-19 14:12:48 +0530

    Added

--- NumsAnsMaths.js
+++ NumsAnsMaths.js
@@ -18,5 +18,5 @@ console.log(Math.min(4,1,2,9));
 console.log(Math.floor(Math.random()*10)); 
 const min=10
 const max=20
-console.log(Math.floor(Math.random()*(max-min+1)))
+console.log(Math.floor(Math.random()*(max-min+1))+min)
 console.log(Math.floor(Math.random()*(max-min+1))+min)
