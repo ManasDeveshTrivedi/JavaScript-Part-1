a89f514720d2b4b42be412ebc61db63a0420a46e
Manas <manasdevtrivedi@gmail.com>
2026-03-19 14:50:27 +0530

    Added

---
+++ Arrays.js
@@ -0,0 +1,28 @@
+// const myArr=[10,1,2,3,4,5]
+// console.log(myArr["one"]);
+// console.log(myArr[0]);
+// const heros=['Ironman','Superman','Spiderman']
+// console.log(heros[1]);
+const myArr1=new Array(0,1,2,3,4,5)
+// console.log(myArr1[3]);
+// myArr1.push(16)
+// myArr1.push(86)
+// console.log(myArr1);
+// myArr1.pop()
+// console.log(myArr1);
+// myArr1.unshift(5)
+// myArr1.shift()
+// console.log(myArr1.includes(9));
+const newArr=myArr1.join()
+// console.log(myArr1);
+// console.log(typeof newArr);
+// const myn1=myArr1.slice(1,3)
+// console.log(myn1);
+// console.log("B",myArr1);
+// const myn2=myArr1.splice(1,3)
+// console.log(myn2);
+// console.log("A",myArr1);
+let arr = [10, 20, 30, 40, 50];
+let result = arr.slice(1, 4);
+console.log(result); // [20, 30, 40]
+console.log(arr);    // original same rahega
