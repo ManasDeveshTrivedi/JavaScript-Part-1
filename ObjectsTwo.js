c1ebacbea3663a70898d4aca1d8cb819365d3b44
Manas <manasdevtrivedi@gmail.com>
2026-03-20 11:52:20 +0530

    Added

--- ObjectsTwo.js
+++ ObjectsTwo.js
@@ -1,4 +1,4 @@
-const TinderUser=new Object()
+//const TinderUser=new Object()
 const tinderUser={}
 tinderUser.id="123abc"
 tinderUser.name="Moye Moye"
@@ -23,6 +23,26 @@ const regularUser={
 const obj1={1:"a",2:"b"}
 const obj2={3:"a",4:"b"}
 //const obj3={obj1,obj2}
-const obj3=Object.assign({},obj1,obj2)
+//const obj3=Object.assign({},obj1,obj2)
+//Assign ke through hum multiple objects assign kar skte hai
 //{} humne iss liye use kiya taaki hum empty obj dikha paye nhi toh saari values obj1 main jayengi
-console.log(obj3);
+const obj3={...obj1,...obj2}
+//console.log(obj3);
+//Array main multiple objects ho skte hai
+const users=[
+    {
+        mail:"manas@gmail.in"
+    },
+    {
+        Age:21
+    },
+    {
+        Number:8303913317
+    }
+]
+//console.log(users[1]);
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));//all the keys created inside tinderUser are shown
+console.log(Object.values(tinderUser));//all the values for the key
+console.log(Object.entries(tinderUser));//key: value format entries
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
