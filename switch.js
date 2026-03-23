25bf418c579c9e22d5956c133e47cedaef7619d5
Manas <manasdevtrivedi@gmail.com>
2026-03-23 10:08:29 +0530

    Added

---
+++ switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+month=111;
+
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 11:
+        console.log("Nov");
+        break;
+
+    default:
+        console.log("Hatt");
+        break;
+}
+
+// let score=200
+// console.log(typeof score);
+// console.log(typeof (score));
+
