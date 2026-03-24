6d0de90a6e6a7b9c8276e3ab57b48321e4b1aa4f
Manas <manasdevtrivedi@gmail.com>
2026-03-24 22:54:15 +0530

    Added

---
+++ truth.js
@@ -0,0 +1,33 @@
+// const email=[]
+// if(email){
+//     console.log("User email");
+// }else{
+//     console.log("Don't have user email");
+//}
+// falsy values:
+// false, 0, -0, BigInt 0n, "", null, undefined, Nan
+//they give or return false always
+
+// truthy values:
+// "0", 'false', " ", [], {}, function(){}
+// function(){} empty function
+
+// if(email===0){
+//     console.log("Array empty");
+// }
+// if(email.length===0){
+//     console.log("Array empty");
+// }
+
+// const emptyObj={}
+// if(Object.keys(emptyObj).length === 0){
+//     console.log("Object is empty");
+    
+// }
+
+//Nullish Coalescing Operator (??):null undefined
+
+let val1;
+val1=5 ?? 10
+
+console.log(val1);
