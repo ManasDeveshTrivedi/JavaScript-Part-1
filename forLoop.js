bfb3d782164f1acc1b7656b13e9546f153f58080
Manas <manasdevtrivedi@gmail.com>
2026-03-25 22:27:46 +0530

    Added

--- forLoop.js
+++ forLoop.js
@@ -1,9 +1,9 @@
-// for (let i = 0; i < 10; i++) {
-//     console.log(`Outer Loops Running: ${i}`)
-//     for (let j = 0; j < 10; j++) {
-//         console.log(`Inner Loops Running: ${j}`)        
-//     }
-// }
+for (let i = 0; i < 10; i++) {
+    console.log(`Outer Loops Running: ${i}`)
+    for (let j = 0; j < 10; j++) {
+        console.log(`Inner Loops Running: ${j}`)        
+    }
+}
 const arr=[4,5,6,9,8,4,3,5]
 for (let i = 0; i < arr.length; i++) {
     if(arr[i]==8){
