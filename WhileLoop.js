c2723ef30cf21122fecdea1b2e8b193b56818d98
Manas <manasdevtrivedi@gmail.com>
2026-03-25 22:38:30 +0530

    Added

---
+++ WhileLoop.js
@@ -0,0 +1,12 @@
+let index=0
+while (index<=20) {
+    console.log(`Value of index is ${index}`);
+    index=index+2
+}
+
+const myarr=['batman','superman','flash']
+let arr=0
+while(arr<myarr.length){
+    console.log(`Value is ${myarr[arr]}`);
+    arr=arr+1
+}
