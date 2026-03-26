5b65450b95dfa7c833236d2d14b409d84f4a88ac
Manas <manasdevtrivedi@gmail.com>
2026-03-26 11:43:17 +0530

    Added

--- Arrays.js
+++ Arrays.js
@@ -3,7 +3,7 @@
 // console.log(myArr[0]);
 // const heros=['Ironman','Superman','Spiderman']
 // console.log(heros[1]);
-const myArr1=new Array(0,1,2,3,4,5)
+//const myArr1=new Array(0,1,2,3,4,5)
 // console.log(myArr1[3]);
 // myArr1.push(16)
 // myArr1.push(86)
@@ -13,7 +13,7 @@ const myArr1=new Array(0,1,2,3,4,5)
 // myArr1.unshift(5)
 // myArr1.shift()
 // console.log(myArr1.includes(9));
-const newArr=myArr1.join()
+//const newArr=myArr1.join()
 // console.log(myArr1);
 // console.log(typeof newArr);
 // const myn1=myArr1.slice(1,3)
@@ -23,6 +23,13 @@ const newArr=myArr1.join()
 // console.log(myn2);
 // console.log("A",myArr1);
 let arr = [10, 20, 30, 40, 50];
-let result = arr.slice(1, 4);
-console.log(result); // [20, 30, 40]
-console.log(arr);    // original same rahega
+//let result = arr.slice(1, 4);
+// console.log(result); // [20, 30, 40]
+// console.log(arr);    // original same rahega
+// let myTotal=arr.reduce(function(acc,currval){
+//     console.log(`acc:${acc} and currval: ${currval}`);
+//     return acc+currval
+// },0)
+
+const myTotal=arr.reduce((acc,curr)=>acc+curr,0)
+console.log(myTotal);
